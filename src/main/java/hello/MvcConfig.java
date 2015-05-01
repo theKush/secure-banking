dd3f906e4af732cb297134879217ddd7dac19bf3
Kush Patel <kushpatel@outlook.com>
2015-05-01 10:59:12 -0500

    basic application setup

---
+++ src/main/java/hello/MvcConfig.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/balance").setViewName("balance");
+        registry.addViewController("/transfer").setViewName("transfer");
+        registry.addViewController("/profile").setViewName("profile");
+    }
+
+}
