7e2f1f37fb96d84243d997f5cbe064d852b3c0d0
Nicholas Rodriguez <nrodriguez3364@gmail.com>
2015-05-04 10:17:20 -0500

    Hashing Transfer Funds

---
+++ src/main/java/hello/MD5.java
@@ -0,0 +1,25 @@
+package hello;
+
+import java.io.FileInputStream;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5 {
+
+    public static String getMD5(String md5) {
+      try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+              sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+           }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+        }
+        return "null";
+    }
+ 
+}
--- src/main/java/hello/TransferController.java
+++ src/main/java/hello/TransferController.java
@@ -5,14 +5,67 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import javax.servlet.http.HttpServletRequest;
+import java.security.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 @Controller
 public class TransferController {
 	
 	@RequestMapping(value = "/transfer", method= RequestMethod.POST)
-	public String transferSubmit(@ModelAttribute Transfer transfer, Model model){
+	public String transferSubmit(@ModelAttribute Transfer transfer, Model model, HttpServletRequest request){
 		
+		String[] tokens = new String[3];
+
+		if (request != null)
+		{
+			try
+			{
+				String tempInput = getStringFromInputStream(request.getInputStream());
+				tokens = tempInput.split("&");
+			}
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			} 
+		}
+
+		String calHashFromAccount = "0";
+		String calHashToAccount = "0";
+		String calHashAmount = "0";
+
+		if (tokens != null)
+		{
+			transfer.setFromAccount(Long.parseLong(tokens[0].replaceAll(".*=", "")));
+			transfer.setToAccount(Long.parseLong(tokens[1].replaceAll(".*=", "")));
+			transfer.setAmount(Integer.parseInt(tokens[2].replaceAll(".*=", "")));
+
+			System.out.println("\n Amount:     " + transfer.getAmount());
+			System.out.println(" From Acount: " + transfer.getFromAccount());
+			System.out.println(" To Acount:   " + transfer.getToAccount());		
+
+			calHashFromAccount = MD5.getMD5(tokens[0].replaceAll(".*=", ""));
+			calHashToAccount = MD5.getMD5(tokens[1].replaceAll(".*=", ""));
+			calHashAmount = MD5.getMD5(tokens[2].replaceAll(".*=", ""));
+		}
+
+		String hashAmount = request.getHeader("hashAmount");
+		String hashFromAccount = request.getHeader("hashFromAccount");
+		String hashToAccount = request.getHeader("hashToAccount");
+		
+		System.out.println( "\n Amount FrontEnd: " +hashAmount);
+		System.out.println( " Amount BackEnd : " +calHashAmount);
+
+		System.out.println( "\n Amount FrontEnd: " +hashToAccount);
+		System.out.println( " Amount BackEnd : " +calHashToAccount);
+
+		System.out.println( "\n Amount FrontEnd: " +hashFromAccount);
+		System.out.println( " Amount BackEnd : " +calHashFromAccount + "\n");
+
 		if (((transfer.getFromAccount() != User.checkingAccount) && (transfer.getFromAccount() != User.savingAccount )) || ((transfer.getToAccount() != User.checkingAccount) && (transfer.getToAccount() != User.savingAccount ))) {
 			model.addAttribute("error", "Account Numbers not correct");
 			return "transfer";
@@ -37,8 +90,17 @@ public class TransferController {
 				model.addAttribute("error", "Not enough balance in your account");
 				return "transfer";
 			}
-		} 
-		
+		}
+//----------------------------- SECURITY CHECK FOR TRANSFER FUNDS -----------------------------------
+		else if ((hashAmount != calHashAmount) ||
+			(hashFromAccount != calHashFromAccount) ||
+			(hashToAccount != calHashToAccount))
+		{
+			model.addAttribute("error", "An Internal Error Occurred");
+				return "transfer";
+		}
+//---------------------------------- END OF SECURITY CHECK  ----------------------------------------
+
 		if (transfer.getFromAccount() == User.checkingAccount) {
 			if (transfer.getToAccount() == User.savingAccount) {
 				if (transfer.getAmount() > 0) {
@@ -68,4 +130,35 @@ public class TransferController {
 		return "transfer";
 	}
 	
+		
+	private static String getStringFromInputStream(InputStream is) {
+		 
+		BufferedReader br = null;
+		StringBuilder sb = new StringBuilder();
+		 
+		String line;
+		try {
+		 
+			br = new BufferedReader(new InputStreamReader(is));
+			while ((line = br.readLine()) != null) {
+			sb.append(line);
+		}
+		 
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+		finally {
+			if (br != null) {
+				try {
+						br.close();
+					} 
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return sb.toString();	 
+	}
+
 }
